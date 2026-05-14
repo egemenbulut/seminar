e259e63fd0168026ef24cfe6b1ffbffcf3726179
egemen <egemen.bulut.2004@gmail.com>
2026-05-14 23:29:36 +0200

    I added how I created the ucb2_alpha_comparison_ratio.png and ucb2_alpha_comparison_regret.png as a comment in the main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from seminar import theorems
 import theorems
+import simulation
+import graphs
 
 data1 = theorems.Theorems(2, [0.9, 0.6])
 data2 = theorems.Theorems(2, [0.9, 0.8])
@@ -8,3 +9,24 @@ data4 = theorems.Theorems(10, [0.9, 0.6])
 data5 = theorems.Theorems(10, [0.9, 0.8, 0.8, 0.8, 0.7, 0.7, 0.7, 0.6])
 data6 = theorems.Theorems(10, [0.9, 0.8])
 data7 = theorems.Theorems(10, [0.55, 0.45])
+
+"""
+# UCP2 with different alpha values
+
+ucb2_1 = simulation.run_simulations(5, data2, "ucb2", alpha=0.1)
+ucb2_01 = simulation.run_simulations(5, data2, "ucb2", alpha=0.01)
+ucb2_001 = simulation.run_simulations(5, data2, "ucb2", alpha=0.001)
+ucb2_0001 = simulation.run_simulations(5, data2, "ucb2", alpha=0.0001)
+
+graphs.multi_regret_plot([(ucb2_1[0], ucb2_1[1], "UCB2 α = 0.1"), 
+    (ucb2_01[0], ucb2_01[1], "UCB2 α = 0.01"), 
+    (ucb2_001[0], ucb2_001[1], "UCB2 α = 0.001"), 
+    (ucb2_0001[0], ucb2_0001[1], "UCB2 α = 0.0001")], 
+    title = "UCB2 Alpha Comparison", filename = "ucb2_alpha_comparison_regret")
+
+graphs.multi_best_machine_ratio_plot([(ucb2_1[0], ucb2_1[2], "UCB2 α = 0.1"), 
+    (ucb2_01[0], ucb2_01[2], "UCB2 α = 0.01"), 
+    (ucb2_001[0], ucb2_001[2], "UCB2 α = 0.001"), 
+    (ucb2_0001[0], ucb2_0001[2], "UCB2 α = 0.0001")], 
+    title = "UCB2 Alpha Comparison", filename = "ucb2_alpha_comparison_ratio")    
+"""
