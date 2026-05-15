ffb5cf6174dd13b65ec8f9a5f43affdfd91e2a40
Ali Osman Kaya <go34xad@mytum.de>
2026-05-15 15:49:24 +0200

    en greedy graphs for dist_2

--- main.py
+++ main.py
@@ -30,3 +30,33 @@ graphs.multi_best_machine_ratio_plot([(ucb2_1[0], ucb2_1[2], "UCB2 α = 0.1"),
     (ucb2_0001[0], ucb2_0001[2], "UCB2 α = 0.0001")], 
     title = "UCB2 Alpha Comparison", filename = "ucb2_alpha_comparison_ratio")    
 """
+
+"""
+eng_005 = simulation.run_simulations(5, data2, "en_greedy", c=0.05, d=0.1)
+eng_010 = simulation.run_simulations(5, data2, "en_greedy", c=0.10, d=0.1)
+eng_015 = simulation.run_simulations(5, data2, "en_greedy", c=0.15, d=0.1)
+eng_020 = simulation.run_simulations(5, data2, "en_greedy", c=0.20, d=0.1)
+eng_025 = simulation.run_simulations(5, data2, "en_greedy", c=0.25, d=0.1)
+eng_030 = simulation.run_simulations(5, data2, "en_greedy", c=0.30, d=0.1)
+
+graphs.multi_regret_plot([
+    (eng_005[0], eng_005[1], "ε-GREEDY c=0.05"),
+    (eng_010[0], eng_010[1], "ε-GREEDY c=0.10"),
+    (eng_015[0], eng_015[1], "ε-GREEDY c=0.15"),
+    (eng_020[0], eng_020[1], "ε-GREEDY c=0.20"),
+    (eng_025[0], eng_025[1], "ε-GREEDY c=0.25"),
+    (eng_030[0], eng_030[1], "ε-GREEDY c=0.30"),
+], title="ε-GREEDY c Comparison (Dist 2)", filename="en_greedy_c_dist2_regret")
+
+graphs.multi_best_machine_ratio_plot([
+    (eng_005[0], eng_005[1], "ε-GREEDY c=0.05"),
+    (eng_010[0], eng_010[1], "ε-GREEDY c=0.10"),
+    (eng_015[0], eng_015[1], "ε-GREEDY c=0.15"),
+    (eng_020[0], eng_020[1], "ε-GREEDY c=0.20"),
+    (eng_025[0], eng_025[1], "ε-GREEDY c=0.25"),
+    (eng_030[0], eng_030[1], "ε-GREEDY c=0.30"),
+], title="ε-GREEDY c Comparison (Dist 2)", filename="en_greedy_c_dist2_ratio")
+
+print("Done.")
+
+"""
